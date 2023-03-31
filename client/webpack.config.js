2dbc3e74a3345ab03822de4d6f3a478580790950
Patrick <patrick@Patricks-iMac.local>
2023-03-31 08:40:44 -0700

    add gitignore configure webpack

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -18,12 +18,44 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Webpack Plugin',
+      }),
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'service-worker.js',
+      }),
+      new WebpackPwaManifest({
+        name: 'Just Another Text Editor',
+        short_name: 'Jate',
+        description: 'Simple text editor Progressive Web App',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons')
+          },
+        ]
+      })
     ],
 
     module: {
       rules: [
-        
+          {
+            test: /\.css$/i,
+            use: ["style-loader", 'css-loader'],
+          },
+          {
+            test: /\.m?js$/,
+            exclude: /(node_modules|bower_components)/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                presets: ['@babel/preset-env'],
+              },
+            },
+          },
       ],
     },
   };
